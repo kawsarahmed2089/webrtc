d485fa42eb71c5feef988c33323c64100d5e53a8
kawsarahmed2089 <kawsarahmed2089@gmail.com>
2020-06-21 11:03:16 +0600

    First Commit

--- js/config.js
+++ js/config.js
@@ -13,6 +13,6 @@ const spinnerClass = 'fa fa-spinner faa-spin animated';
 
 
 
-const appRoot = 'http://34.75.56.165/webrtc2/';
+const appRoot = 'http://34.75.56.165/webrtc/';
 const wsUrl = 'ws://34.75.56.165:62434'; //use wss://localhost:8080/comm for secured connection
 const spinnerClass = 'fa fa-spinner faa-spin animated';
