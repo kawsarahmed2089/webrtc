85fa2c0c434a32b46d2e00f92076c2bc3f0a6939
kawsarahmed2089 <kawsarahmed2089@gmail.com>
2020-06-21 10:46:52 +0600

    First Commit

---
+++ js/config.js
@@ -0,0 +1,18 @@
+/**
+ * @author Amir Sanni <amirsanni@gmail.com>
+ * @date 23-Dec-2016
+ */
+
+'use strict';
+
+/*
+const appRoot = 'https://localhost/amir/';
+const wsUrl = 'ws://localhost:62434'; //use wss://localhost:8080/comm for secured connection
+const spinnerClass = 'fa fa-spinner faa-spin animated';
+*/
+
+
+
+const appRoot = 'http://34.75.56.165/webrtc2/';
+const wsUrl = 'ws://34.75.56.165:62434'; //use wss://localhost:8080/comm for secured connection
+const spinnerClass = 'fa fa-spinner faa-spin animated';
---
+++ server.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Description of server
+ *
+ * @author Amir <amirsanni@gmail.com>
+ * @date 23-Dec-2016
+ */
+
+require 'vendor/autoload.php';
+
+use Amir\Comm;
+use Ratchet\App;
+
+//set an array of origins allowed to connect to this server
+$allowed_origins = ['localhost', '34.75.56.165','62434'];
+
+// Run the server application through the WebSocket protocol on port 8080
+$app = new App('localhost', '62434', '34.75.56.165');//App(hostname, port, 'whoCanConnectIP', '')
+
+//create socket routes
+//route(uri, classInstance, arrOfAllowedOrigins)
+$app->route('/comm', new Comm, $allowed_origins);
+
+//run websocket
+$app->run();
